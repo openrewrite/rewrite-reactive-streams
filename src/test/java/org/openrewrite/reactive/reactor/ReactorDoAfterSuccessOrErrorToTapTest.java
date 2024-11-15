55e4148e33991b06028a74b876c8d801eae261b7
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-11-15 19:30:12 +0100

    Migrate `doAfterSuccessOrError` to `tap` (#9)
    * WIP
    
    * WIP
    
    * FMT
    
    * Fix findings
    
    * Add test coverage
    WIP
    
    * WIP
    
    * Separate templates works better but still missing type information due to tap only existing from 3.5
    
    * Attempt to resolve type issue
    
    * Add both reactor-core-3.4 and reactor-core-3.5 at classpath in test
    
    * Add reactive-streams to classpath
    
    * Fix broken ReactorDoAfterSuccessOrErrorToTap
    
    * Make else blocks optional
    
    * Code cleanup
    
    * Change template + code after review of Oleh
    
    * Change type of Mono in `emptyLambda` to prove it works with different generic types as well
    
    * Improve template to fix possible concurrency issues
    
    * Revert recipeDependencies->parserClasspath -> io.projectreactor:reactor-core:3.5.20
    
    * Apply suggestions from code review
    
    Co-authored-by: Laurens Westerlaken <laurens.w@live.nl>
    
    * Rerevert recipeDependencies->parserClasspath -> io.projectreactor:reactor-core:3.5.20
    
    * Apply formatter and add language hint; inline method
    
    * Move `ReactorDoAfterSuccessOrErrorToTap` up (more logical place)
    
    * Demonstrate class cast exception
    
    * Fix breaking recipe when doAfterSuccessOrError receives a variable as argument
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix breaking recipe when doAfterSuccessOrError receives a variable as argument
    
    * Prune template
    
    * Add missing `return` statement in template
    
    * Light polish
    
    * Do not hardcode consumer variable name; handle `new MyBiConsumer()`
    
    ---------
    
    Co-authored-by: lingenj <jacob.van.lingen@moderne.io>
    Co-authored-by: Jacob van Lingen <jacobvanlingen@hotmail.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/org/openrewrite/reactive/reactor/ReactorDoAfterSuccessOrErrorToTapTest.java
@@ -0,0 +1,338 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.reactive.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.SourceSpec;
+import org.openrewrite.test.TypeValidation;
+
+import static org.openrewrite.java.Assertions.java;
+
+class ReactorDoAfterSuccessOrErrorToTapTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "reactor-core-3.4", "reactive-streams"))
+          .recipe(new ReactorDoAfterSuccessOrErrorToTap());
+    }
+
+    @Test
+    @DocumentExample
+    void refactorSuccessfully() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              import reactor.core.publisher.Mono;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono) {
+                      mono.doAfterSuccessOrError((result, error) -> {
+                          if (error != null) {
+                              System.out.println("error" + error);
+                          } else {
+                              System.out.println("success" + result);
+                          }
+                          System.out.println("other logs");
+                      }).subscribe();
+                  }
+              }
+              """,
+            """
+              import reactor.core.observability.DefaultSignalListener;
+              import reactor.core.publisher.Mono;
+              import reactor.core.publisher.Operators;
+              import reactor.core.publisher.SignalType;
+              import reactor.util.context.Context;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono) {
+                      mono.tap(() -> new DefaultSignalListener<>() {
+                          String result;
+                          Throwable error;
+                          boolean done;
+                          boolean processedOnce;
+                          Context currentContext;
+
+                          @Override
+                          public synchronized void doFinally(SignalType signalType) {
+                              if (processedOnce) {
+                                  return;
+                              }
+                              processedOnce = true;
+                              if (signalType == SignalType.CANCEL) {
+                                  return;
+                              }
+                              if (error != null) {
+                                  System.out.println("error" + error);
+                              } else {
+                                  System.out.println("success" + result);
+                              }
+                              System.out.println("other logs");
+                          }
+
+                          @Override
+                          public synchronized void doOnNext(String result) {
+                              if (done) {
+                                  Operators.onDiscard(result, currentContext);
+                                  return;
+                              }
+                              this.result = result;
+                          }
+
+                          @Override
+                          public synchronized void doOnComplete() {
+                              this.done = true;
+                          }
+
+                          @Override
+                          public synchronized void doOnError(Throwable error) {
+                              if (done) {
+                                  Operators.onErrorDropped(error, currentContext);
+                                  return;
+                              }
+                              this.error = error;
+                              this.done = true;
+                          }
+
+                          @Override
+                          public Context addToContext(Context originalContext) {
+                              currentContext = originalContext;
+                              return originalContext;
+                          }
+
+                          @Override
+                          public synchronized void doOnCancel() {
+                              if (done) {
+                                  return;
+                              }
+                              this.done = true;
+                              if (result != null) {
+                                  Operators.onDiscard(result, currentContext);
+                              }
+                          }
+                      }).subscribe();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void refactorSuccessfullyWhenBiConsumerVariableIsUsed() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              import java.util.function.BiConsumer;
+              import reactor.core.publisher.Mono;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono, BiConsumer<String, Throwable> biConsumer) {
+                      mono.doAfterSuccessOrError(biConsumer).subscribe();
+                  }
+              }
+              """,
+            """
+              import java.util.function.BiConsumer;
+
+              import reactor.core.observability.DefaultSignalListener;
+              import reactor.core.publisher.Mono;
+              import reactor.core.publisher.Operators;
+              import reactor.core.publisher.SignalType;
+              import reactor.util.context.Context;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono, BiConsumer<String, Throwable> biConsumer) {
+                      mono.tap(() -> new DefaultSignalListener<>() {
+                          String result;
+                          Throwable error;
+                          boolean done;
+                          boolean processedOnce;
+                          Context currentContext;
+
+                          @Override
+                          public synchronized void doFinally(SignalType signalType) {
+                              if (processedOnce) {
+                                  return;
+                              }
+                              processedOnce = true;
+                              if (signalType == SignalType.CANCEL) {
+                                  return;
+                              }
+                              biConsumer.accept(result, error);
+                          }
+
+                          @Override
+                          public synchronized void doOnNext(String result) {
+                              if (done) {
+                                  Operators.onDiscard(result, currentContext);
+                                  return;
+                              }
+                              this.result = result;
+                          }
+
+                          @Override
+                          public synchronized void doOnComplete() {
+                              this.done = true;
+                          }
+
+                          @Override
+                          public synchronized void doOnError(Throwable error) {
+                              if (done) {
+                                  Operators.onErrorDropped(error, currentContext);
+                                  return;
+                              }
+                              this.error = error;
+                              this.done = true;
+                          }
+
+                          @Override
+                          public Context addToContext(Context originalContext) {
+                              currentContext = originalContext;
+                              return originalContext;
+                          }
+
+                          @Override
+                          public synchronized void doOnCancel() {
+                              if (done) {
+                                  return;
+                              }
+                              this.done = true;
+                              if (result != null) {
+                                  Operators.onDiscard(result, currentContext);
+                              }
+                          }
+                      }).subscribe();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void refactorSuccessfullyWhenNewBiConsumerIsUsed() {
+        //language=java
+        rewriteRun(
+          // The BiConsumer is defined in a separate file, yet we need the classpath from resources for reactor 3.5.+
+          spec -> spec.afterTypeValidationOptions(TypeValidation.all().methodInvocations(false)),
+          java(
+            """
+              import java.util.function.BiConsumer;
+              class MyBiConsumer implements BiConsumer<String, Throwable> {
+                  @Override
+                  public void accept(String s, Throwable throwable) {
+                      System.out.println(s);
+                  }
+              }
+              """,
+            SourceSpec::skip
+          ),
+          java(
+            """
+              import reactor.core.publisher.Mono;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono) {
+                      mono.doAfterSuccessOrError(new MyBiConsumer()).subscribe();
+                  }
+              }
+              """,
+            """
+              import reactor.core.observability.DefaultSignalListener;
+              import reactor.core.publisher.Mono;
+              import reactor.core.publisher.Operators;
+              import reactor.core.publisher.SignalType;
+              import reactor.util.context.Context;
+
+              class SomeClass {
+                  void doSomething(Mono<String> mono) {
+                      mono.tap(() -> new DefaultSignalListener<>() {
+                          String result;
+                          Throwable error;
+                          boolean done;
+                          boolean processedOnce;
+                          Context currentContext;
+
+                          @Override
+                          public synchronized void doFinally(SignalType signalType) {
+                              if (processedOnce) {
+                                  return;
+                              }
+                              processedOnce = true;
+                              if (signalType == SignalType.CANCEL) {
+                                  return;
+                              }
+                              new MyBiConsumer().accept(result, error);
+                          }
+
+                          @Override
+                          public synchronized void doOnNext(String result) {
+                              if (done) {
+                                  Operators.onDiscard(result, currentContext);
+                                  return;
+                              }
+                              this.result = result;
+                          }
+
+                          @Override
+                          public synchronized void doOnComplete() {
+                              this.done = true;
+                          }
+
+                          @Override
+                          public synchronized void doOnError(Throwable error) {
+                              if (done) {
+                                  Operators.onErrorDropped(error, currentContext);
+                                  return;
+                              }
+                              this.error = error;
+                              this.done = true;
+                          }
+
+                          @Override
+                          public Context addToContext(Context originalContext) {
+                              currentContext = originalContext;
+                              return originalContext;
+                          }
+
+                          @Override
+                          public synchronized void doOnCancel() {
+                              if (done) {
+                                  return;
+                              }
+                              this.done = true;
+                              if (result != null) {
+                                  Operators.onDiscard(result, currentContext);
+                              }
+                          }
+                      }).subscribe();
+                  }
+              }
+              """
+          )
+        );
+    }
+}
